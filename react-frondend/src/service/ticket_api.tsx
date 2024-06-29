d063a042554b894818f221d8e1932dff5dd4b721
Arm Nonthakon <nonthakon.t3@gmail.com>
2024-06-29 22:11:26 +0700

    Change url

--- react-frondend/src/service/ticket_api.tsx
+++ react-frondend/src/service/ticket_api.tsx
@@ -3,9 +3,7 @@ import { getCookie } from "typescript-cookie";
 import parseJwt from '../utils/decode';
 
 
-const url = 'http://127.0.0.1:3000/'
-
-
+const url = '/api/'
 export const ReserveSeat = async (MovieId: string, TheaterId: string, seatReserve: string[]) => {
     const jwt = getCookie("token") as string
     const decoded = parseJwt(jwt)
